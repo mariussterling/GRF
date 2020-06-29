511c53d06bee04124d7582a971a0dac3e720426d
quantletteam <quantlet.wiwi@hu-berlin.de>
2020-06-29 15:12:27 +0200

    init

---
+++ huberized.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 14:13:57 2020
+
+@author: rdc
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+
+def loss_huber(x, k, tau=0.5):
+    res = (np.abs(x) <= k) * x ** 2
+    res += (np.abs(x) > k) * (2 * k * np.abs(x) - k**2)
+    res *= np.abs((x < 0) - tau)
+    return(res)
+
+
+def score_huber(x, k, tau=0.5):
+    res = (np.abs(x) <= k) * x * 2
+    res += (np.abs(x) > k) * (2 * k * np.sign(x))
+    res *= np.abs((x < 0) - tau)
+    return(res)
+
+
+def score_huber_deriv(x, k, tau=0.5):
+    res = x*0
+    res += (np.abs(x) <= k) * 2
+    res = res.astype('float64')
+    res *= np.abs((x < 0) * 1 - tau)
+    return(res)
+
+
+k = 0.7
+x = np.arange(-4, 4, 0.0001)
+
+
+fig, ax = plt.subplots(ncols=3, figsize=(8, 4), dpi=150)
+ax[0].plot(x, loss_huber(x, k))
+ax[0].yaxis.set_major_locator(MaxNLocator(integer=True))
+ax[0].set(xlabel=r'$u$', ylabel=r"$\rho_H(u,k={0})$".format(k))
+
+
+ax[1].plot(x, score_huber(x, k))
+ax[1].set_yticks([-k, 0, k])
+ax[1].set(xlabel=r'$u$', ylabel=r"$\psi_H(u,k={0})$".format(k))
+
+
+ax[2].plot(x, score_huber_deriv(x, k))
+ax[2].yaxis.set_major_locator(MaxNLocator(integer=True))
+ax[2].set(xlabel=r'$u$', ylabel=r"$\psi^\prime_H(u,k={0})$".format(k))
+
+plt.subplots_adjust(wspace=1)
+plt.savefig(
+        fname='huberized.png',
+        dpi=150,
+        transparent=True
+)
+
+
+# %% Asymmetric Huberized loss
+
+tau = 0.2
+
+fig, ax = plt.subplots(ncols=3, figsize=(8, 4), dpi=150)
+ax[0].plot(x, loss_huber(x, k, tau))
+ax[0].yaxis.set_major_locator(MaxNLocator(integer=True))
+ax[0].set(xlabel=r'$u$', ylabel=r"$\rho_{AH,%.1f}(u,k=%.1f)$"%(tau, k))
+
+
+ax[1].plot(x, score_huber(x, k, tau))
+ax[1].set_yticks([-k * (1 - tau) * 2, 0, k * tau * 2])
+ax[1].set(xlabel=r'$u$', ylabel=r"$\psi_{AH,%.1f}(u,k=%.1f)$"%(tau, k))
+
+
+
+ax[2].plot(x, score_huber_deriv(x, k, tau))
+ax[2].set_yticks([2*(1 - tau), 0, 2*tau])
+ax[2].set(xlabel=r'$u$', ylabel=r"$\psi^\prime_{AH,%.1f}(u,k=%.1f)$"%(tau, k))
+
+
+plt.subplots_adjust(wspace=1)
+plt.savefig(
+        fname='huberized_asymmetric.png',
+        dpi=150,
+        transparent=True
+ )
+plt.show()
+
