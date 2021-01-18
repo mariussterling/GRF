8abfe1d9e42a6f2daff492ca8f781b4909847ce6
quantletteam <quantlet.wiwi@hu-berlin.de>
2021-01-18 11:37:28 +0100

    script faster

--- GRF_effective_weights/GRF_effective_weights.R
+++ GRF_effective_weights/GRF_effective_weights.R
@@ -4,27 +4,23 @@ library(ggplot2)
 library(glue)
 set.seed(42)
 
-sig = 0
+
+get_x = function(n){
+  X = matrix((-(n / 2 - 1):(n / 2)) / (n / 2), nrow = n)
+  colnames(X) = 'X1'
+  return(X)
+}
+theta_triangle = function(x, width){
+  pmax(1 - abs((x) / width), 0)
+}
+
+get_y = function(X, theta, sigma, seed=NULL){
+  set.seed(NULL)
+  n = nrow(X)
+  return(theta(X) + rnorm(n, 0, sigma))
+}
+
 for (sig in c(0, 0.1)){
-  # X = matrix(rnorm(n * p, 0.5, sigma), nrow = n)
-  get_x = function(n){
-    X = matrix((-(n / 2 - 1):(n / 2)) / (n / 2), nrow = n)
-    colnames(X) = 'X1'
-    return(X)
-  }
-  theta_triangle = function(x, width){
-    pmax(1 - abs((x) / width), 0)
-  }
-  
-  get_y = function(X, theta, sigma, seed=NULL){
-    set.seed(NULL)
-    n = nrow(X)
-    return(theta(X) + rnorm(n, 0, sigma))
-  }
-  
-  
-  # n --------------------------------------------------------------------
-  
   rfs = list()
   for (n in c(500, 1000, 2000)){
     width = 0.2
@@ -36,7 +32,7 @@ for (sig in c(0, 0.1)){
     alpha = get_sample_weights(rf)
     theta_tilde = theta(X) + alpha %*% eps_tilde
     
-    png(glue('RF_theta_triangle___theta_tilde___sigma{formatC(sig*100, width=3, flag="0")}_n{formatC(n, width=4, flag="0")}.png'))
+    pdf(file = glue('RF_theta_triangle___theta_tilde___sigma{formatC(sig*100, width=3, flag="0")}_n{formatC(n, width=4, flag="0")}.pdf'))
       plot(X[, 'X1'], Y, type = 'p', pch = 20, cex = 0.25, xlab='X1')
       lines(X[, 'X1'], theta_tilde, col = 'red')
       lines(X[, 'X1'], theta(X), col = 'blue', lty=2)
@@ -44,26 +40,23 @@ for (sig in c(0, 0.1)){
     rfs[[as.character(n)]] = rf
   }
   
-  for (x1 in c(seq(0, 0.3, 0.025), seq(0.4, 1, 0.2))){
-    png(glue('RF_theta_triangle___effective_weights___sigma{formatC(sig*100, width=3, flag="0")}___xi{formatC(x1*1000, width=4, flag="0")}.png'))
-      m = max(sapply(rfs, function(x)max(get_sample_weights(x)[])))
-      rf = rfs[[names(rfs)[1]]]
-      i = which(min(abs(rf$X.orig - x1)) == abs(rf$X.orig - x1))[1]
+  x1s = get_x(100)
+  alpha = lapply(rfs, function(x) get_sample_weights(x, newdata = x1s))
+  rf_X.orig = lapply(rfs, function(x) x$X.orig[, 'X1'])
+  for (i in 1:nrow(x1s)){
+    pdf(file = glue('RF_theta_triangle___effective_weights___sigma{formatC(sig*100, width=3, flag="0")}___xi{formatC(x1s[i]*1000, width=4, flag="0")}.pdf'))
+      # m = max(sapply(rfs, function(x)max(get_sample_weights(x)[])))
       plot(
-        rf$X.orig[-c(i), 'X1'],
-        get_sample_weights(rf)[i, ][-c(i)],
+        rf_X.orig[[1]],
+        alpha[[1]][i, ],
         xlab = 'x', 
         ylab = parse(text = 'alpha[i](x)'),
-        main = bquote('Effective weights'~alpha[i](x) ~'for' ~ X[i] == .(x1)),
+        main = bquote('Effective weights'~alpha[i](x) ~'for' ~ X[i] == .(x1s[i])),
         type = 'l',
         ylim = c(0, 0.1)
       )
-      rf = rfs[[names(rfs)[2]]]
-      i = which(min(abs(rf$X.orig - x1)) == abs(rf$X.orig - x1))[1]
-      lines(rf$X.orig[-c(i), 'X1'], get_sample_weights(rf)[i, ][-c(i)], col = 'blue')
-      rf = rfs[[names(rfs)[3]]]
-      i = which(min(abs(rf$X.orig - x1)) == abs(rf$X.orig - x1))[1]
-      lines(rf$X.orig[-c(i), 'X1'], get_sample_weights(rf)[i, ][-c(i)], col = 'red')
+      lines(rf_X.orig[[2]], alpha[[2]][i, ], col = 'blue')
+      lines(rf_X.orig[[3]], alpha[[3]][i, ], col = 'red')
     dev.off()
   }
 }
